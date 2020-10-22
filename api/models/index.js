ed8ea22500659fae4a39dfec87432a213ce3ab78
Aisar Gatrif <aisargatrif@gmail.com>
2020-10-22 17:41:33 -0400

    added admin seed

--- api/models/index.js
+++ api/models/index.js
@@ -1,110 +1,110 @@
 // ~~ USERS ~~ //
 
 const User = require('./User')
-const UserInfo = require('./UserInfo')
-const UserLocation = require('./UserLocation')
-const UserRating = require('./UserRating')
-const UserVerification = require('./UserVerification')
+// const UserInfo = require('./UserInfo')
+// const UserLocation = require('./UserLocation')
+// const UserRating = require('./UserRating')
+// const UserVerification = require('./UserVerification')
 
 // ~~ COMPANIES ~~ //
 
-const Company = require('./Company')
-const CompanyRating = require('./CompanyRating')
-const CompanyInfo = require('./CompanyInfo')
-const CompanyLocation = require('./CompanyLocation')
+// const Company = require('./Company')
+// const CompanyRating = require('./CompanyRating')
+// const CompanyInfo = require('./CompanyInfo')
+// const CompanyLocation = require('./CompanyLocation')
 
 // ~~ PRODUCTS ~~ //
 
-const Product = require('./Product')
-const ProductLogistic = require('./ProductLogistic')
-const ProductIncoTerm = require('./ProductIncoTerm')
-const ProductLeadTime = require('./ProductLeadTime')
-const ProductLocation = require('./ProductLocation')
-const ProductPriceRange = require('./ProductPriceRange')
-const ProductQuickDetail = require('./ProductQuickDetail')
-const ProductUnitType = require('./ProductUnitType')
-const ProductReview = require('./ProductReview')
-const ProductRating = require('./ProductRating')
-const ProductQuestion = require('./ProductQuestion')
-const ProductAnswer = require('./ProductAnswer')
-const ProductImage = require('./ProductImage')
+// const Product = require('./Product')
+// const ProductLogistic = require('./ProductLogistic')
+// const ProductIncoTerm = require('./ProductIncoTerm')
+// const ProductLeadTime = require('./ProductLeadTime')
+// const ProductLocation = require('./ProductLocation')
+// const ProductPriceRange = require('./ProductPriceRange')
+// const ProductQuickDetail = require('./ProductQuickDetail')
+// const ProductUnitType = require('./ProductUnitType')
+// const ProductReview = require('./ProductReview')
+// const ProductRating = require('./ProductRating')
+// const ProductQuestion = require('./ProductQuestion')
+// const ProductAnswer = require('./ProductAnswer')
+// const ProductImage = require('./ProductImage')
 
 // ~~ SHOPPING CARTS ~~ //
 
-const ShoppingCart = require('./ShoppingCart')
-const ShoppingCartProduct = require('./ShoppingCartProduct')
+// const ShoppingCart = require('./ShoppingCart')
+// const ShoppingCartProduct = require('./ShoppingCartProduct')
 
 // ~~ ORDERS  ~~ //
 
-const Order = require('./Order')
-const OrderProduct = require('./OrderProduct')
+// const Order = require('./Order')
+// const OrderProduct = require('./OrderProduct')
 
 // ~~ REVIEWS ~~ //
 
-const SellerReview = require('./SellerReview')
-const BuyerReview = require('./BuyerReview')
+// const SellerReview = require('./SellerReview')
+// const BuyerReview = require('./BuyerReview')
 
 // ~~ LOCATIONS  ~~ //
 
-const Region = require('./Region')
-const Subregion = require('./Subregion')
-const Country = require('./Country')
-const State = require('./State')
+// const Region = require('./Region')
+// const Subregion = require('./Subregion')
+// const Country = require('./Country')
+// const State = require('./State')
 
 // ~~ BLOG  ~~ //
 
-const Post = require('./Post')
-const Comment = require('./Comment')
+// const Post = require('./Post')
+// const Comment = require('./Comment')
 
 // ~~ MISCELLANEOUS  ~~ //
 
-const Language = require('./Language')
-const Category = require('./Category')
-const Subcategory = require('./Subcategory')
+// const Language = require('./Language')
+// const Category = require('./Category')
+// const Subcategory = require('./Subcategory')
 
 module.exports = [
   ['User', User],
-  ['UserInfo', UserInfo],
-  ['UserLocation', UserLocation],
-  ['UserRating', UserRating],
-  ['UserVerification', UserVerification],
+  // ['UserInfo', UserInfo],
+  // ['UserLocation', UserLocation],
+  // ['UserRating', UserRating],
+  // ['UserVerification', UserVerification],
 
-  ['Company', Company],
-  ['CompanyRating', CompanyRating],
-  ['CompanyInfo', CompanyInfo],
-  ['CompanyLocation', CompanyLocation],
+  // ['Company', Company],
+  // ['CompanyRating', CompanyRating],
+  // ['CompanyInfo', CompanyInfo],
+  // ['CompanyLocation', CompanyLocation],
 
-  ['Product', Product],
-  ['ProductAnswer', ProductAnswer],
-  ['ProductIncoTerm', ProductIncoTerm],
-  ['ProductLeadTime', ProductLeadTime],
-  ['ProductLocation', ProductLocation],
-  ['ProductLogistic', ProductLogistic],
-  ['ProductPriceRange', ProductPriceRange],
-  ['ProductQuestion', ProductQuestion],
-  ['ProductQuickDetail', ProductQuickDetail],
-  ['ProductRating', ProductRating],
-  ['ProductReview', ProductReview],
-  ['ProductUnitType', ProductUnitType],
-  // ['ProductImage', ProductImage],
+  // ['Product', Product],
+  // ['ProductAnswer', ProductAnswer],
+  // ['ProductIncoTerm', ProductIncoTerm],
+  // ['ProductLeadTime', ProductLeadTime],
+  // ['ProductLocation', ProductLocation],
+  // ['ProductLogistic', ProductLogistic],
+  // ['ProductPriceRange', ProductPriceRange],
+  // ['ProductQuestion', ProductQuestion],
+  // ['ProductQuickDetail', ProductQuickDetail],
+  // ['ProductRating', ProductRating],
+  // ['ProductReview', ProductReview],
+  // ['ProductUnitType', ProductUnitType],
+  // // ['ProductImage', ProductImage],
 
-  ['SellerReview', SellerReview],
-  ['BuyerReview', BuyerReview],
+  // ['SellerReview', SellerReview],
+  // ['BuyerReview', BuyerReview],
 
-  ['ShoppingCart', ShoppingCart],
-  ['ShoppingCartProduct', ShoppingCartProduct],
+  // ['ShoppingCart', ShoppingCart],
+  // ['ShoppingCartProduct', ShoppingCartProduct],
 
-  ['Order', Order],
-  ['OrderProduct', OrderProduct],
+  // ['Order', Order],
+  // ['OrderProduct', OrderProduct],
 
-  ['Region', Region],
-  ['Subregion', Subregion],
-  ['Country', Country],
-  ['State', State],
+  // ['Region', Region],
+  // ['Subregion', Subregion],
+  // ['Country', Country],
+  // ['State', State],
 
-  ['Category', Category],
-  ['Subcategory', Subcategory],
-  ['Language', Language],
+  // ['Category', Category],
+  // ['Subcategory', Subcategory],
+  // ['Language', Language],
 
   // ['Post', Post],
   // ['Comment', Comment],
